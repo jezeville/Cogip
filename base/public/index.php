dde59e4bf5b3bc41bf3e8cdeee9ddc15424002cc
jeremy frederic <jeremyfrederic12@gmail.com>
2024-02-27 14:05:03 +0100

    base

---
+++ base/public/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// Please, use php -S localhost:8000 -t public in your terminal to start the project
+
+require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../Core/Helper.php';
+require_once __DIR__.'/../Routes/routes.php';
