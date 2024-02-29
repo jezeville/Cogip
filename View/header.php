928a2f6178ecf50f57017934cc51a3a940f8985b
alex <1alexispetropoulos@gmail.com>
2024-02-29 10:57:13 +0100

    création du header

--- View/header.php
+++ View/header.php
@@ -1,14 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <header>
+<header class="bg-[#F9DE4E]">
+    <div class="flex justify-center items-center pt-20 pb-8">
 
+        <div class="w-1/4">
+            <a class="text-5xl font-bold" href="renvoie la page d'acceuil">COGIP</a>
+        </div>
 
-    </header>           
+        <nav class="w-2/5">
+            <ul class="list-none flex space-x-8">
+                <li class="text-xl font-bold">Home</li>
+                <li class="text-xl font-bold">Invoice</li>
+                <li class="text-xl font-bold">Compagnies</li>
+                <li class="text-xl font-bold">Contacts</li>
+            </ul>
+        </nav>
+
+        <div class="w-1/6 flex space-x-8">
+            <button class="text-xl font-bold rounded-lg px-4 py-2 bg-white">Log in</button>
+            <button class="text-xl font-bold hover:bg-white rounded-lg px-4 py-2 ">Sign Up</button>
+        </div>
+    </div>
+
+    <div class="ml-24 mt-4">
+        <svg width="1299" height="112" viewBox="0 0 1299 112" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 39.3514H649.5L1046 0L1299 112H0V39.3514Z" fill="white" />
+        </svg>
+    </div>
+</header>
 </body>
+
 </html>
