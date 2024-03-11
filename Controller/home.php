bf92e26e60666c1eded3d33088be26282a7ce3ca
Hanen Wechteti <hanenwechteti27@gmail.com>
2024-03-11 09:11:20 +0100

    version3

--- Controller/home.php
+++ Controller/home.php
@@ -2,38 +2,50 @@
 
 require('../Model/connection.php');
 
+class Home
+{
+    private $db;
 
-// function to get 5 last invoices
-function getLatestInvoices($conn, $limit) {
-   
-   
-//    $sql = "SELECT invoices.ref, DATE(invoices.created_at) as created_date, DATE(invoices.updated_at) as updated_date, DATE(invoices.due_date) as due_date, companies.name AS company_name 
-//     FROM invoices
-//     JOIN companies ON invoices.id_company = companies.id
-//     JOIN types ON companies.type_id = types.id
-//     ORDER BY invoices.created_at DESC
-//     LIMIT :offset, :maxPage";
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
 
+    // function to get 5 last invoices
+ // function to get 5 last invoices
+public function getLatestInvoices($limit) {
+    $sql = "SELECT invoices.ref, DATE(invoices.due_date) as due_date, companies.name AS company_name, DATE(invoices.created_at) as created_at  
+        FROM invoices
+        JOIN companies ON invoices.id_company = companies.id
+        JOIN types ON companies.type_id = types.id
+        ORDER BY invoices.created_at DESC LIMIT $limit";
 
-    $sql = "SELECT invoices.ref, DATE(invoices.created_at) as created_date, DATE(invoices.updated_at) as updated_date, DATE(invoices.due_date) as due_date, companies.name AS company_name 
-    FROM invoices ORDER BY date DESC LIMIT $limit";
-    $result = $conn->query($sql);
+    $result = $this->db->query($sql);
 
-    return ($result->num_rows > 0) ? $result->fetch_all(MYSQLI_ASSOC) : [];
+    return ($result->rowCount() > 0) ? $result->fetchAll(PDO::FETCH_ASSOC) : [];
 }
 
-// function to get 5 last contacts
-function getLatestContact($conn, $limit) {
-    $sql = "SELECT first_name, last_name, email FROM people ORDER BY created_at DESC LIMIT $limit";
-    $result = $conn->query($sql);
 
-    return ($result->num_rows > 0) ? $result->fetch_all(MYSQLI_ASSOC) : [];
-}
+    // function to get 5 last contacts
 
-// // function to get 5 last companies
-function getLatestCompanies($conn, $limit) {
-    $sql = "SELECT name, type FROM companies ORDER BY created_at DESC LIMIT $limit";
-    $result = $conn->query($sql);
+    public function getLatestContact($limit) {
+        $sql = "SELECT contacts.name, contacts.phone, contacts.email, companies.name AS company_name, DATE(contacts.created_at) as created_at 
+        FROM contacts 
+        JOIN companies ON contacts.company_id = companies.id
+        ORDER BY created_at DESC LIMIT $limit";
+        $result = $this->db->query($sql);
 
-    return ($result->num_rows > 0) ? $result->fetch_all(MYSQLI_ASSOC) : [];
+        return ($result->rowCount() > 0) ? $result->fetchAll(PDO::FETCH_ASSOC) : [];
+    }
+
+    // function to get 5 last companies
+    public function getLatestCompanies($limit) {
+        $sql = "SELECT companies.name, companies.TVA, companies.country, types.name AS type_name,DATE(companies.created_at) AS created_at 
+        FROM companies 
+        JOIN types ON companies.type_id = types.id
+        ORDER BY created_at DESC LIMIT $limit";
+        $result = $this->db->query($sql);
+
+        return ($result->rowCount() > 0) ? $result->fetchAll(PDO::FETCH_ASSOC) : [];
+    }
 }
